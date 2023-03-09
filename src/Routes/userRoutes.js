efe77aaa67e3b24fa502478dde8c0c3db13e5f1b
pinocchio <officialnikx@gmail.com>
2023-03-09 04:19:24 +0100

    feat: sethub the user login and logout

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const {validateSchema, registrationSchemaValidation} = require('../validation');
+const {validateSchema, registrationSchemaValidation,loginSchemaValidation} = require('../validation');
 const controller = require('../Controllers/userControllers');
 
 router
       .post('/register', validateSchema(registrationSchemaValidation), controller.registerUser)
+      .post('/login', validateSchema(loginSchemaValidation), controller.logInUser)
+      .post('/logout', controller.logOutUser)
 
-      module.exports = router;
+module.exports = router;
