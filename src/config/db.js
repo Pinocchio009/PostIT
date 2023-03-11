31552a178f4bc22a93fa8901c250c8fdcc23c991
pinocchio <81025400+Pinocchio009@users.noreply.github.com>
2023-03-11 21:15:35 +0100

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ config();
 mongoose.set('strictQuery', true);
 async function connect (uri) {
     try {
-        await mongoose.connect(uri || process.env.MONGO_DB_LOCAL)
+        await mongoose.connect(uri || "mongodb://localhost/27017")
         
         console.log('connected to db')
     } catch (error) {
